da2c7eaf3eb9e81053a327be51a216dfa9160027
gemaquejr <gemaquejr@hotmail.com>
2023-02-06 11:08:29 -0300

    Adiciona refatoracao no component about

--- portfolio/src/components/about.jsx
+++ portfolio/src/components/about.jsx
@@ -8,7 +8,7 @@ const About = () => {
       className="bg-gradient-to-b from-gray-800 to-black text-white w-full h-screen"
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-screen">
-        <div className="lg:pb-8">
+        <div className="pt-8">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500 p-2">
             Sobre
           </p>
